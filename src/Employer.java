cd10a7acda9e16850b0d2117fb3a87d9f8df2bf0
deymosko <maximstoyka1@gmail.com>
2024-10-30 18:09:59 +0100

    1.3

--- src/Employer.java
+++ src/Employer.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+public class Employer implements Person
+{
+    private final int AGE;
+    private final String NAME;
+    private final String USERNAME;
 
-public class Employer {
+    public Employer(int age, String name, String username) {
+        AGE = age;
+        NAME = name;
+        USERNAME = username;
+    }
+
+    @Override
+    public void getInfo() {
+        System.out.println("Name: " + NAME);
+        System.out.println("Age: " + AGE);
+        System.out.println("Username: " + USERNAME);
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,22 @@
 public class Student implements Person{
+    private final int STUDENT_ID;
+    private final int AGE;
+    private final String NAME;
+    private final String USERNAME;
+
+    public Student(int studentId, int age, String name, String username) {
+        STUDENT_ID = studentId;
+        AGE = age;
+        NAME = String.valueOf(name);
+        USERNAME = username;
+    }
+
     @Override
-    public void getInfo() {
+    public void getInfo()
+    {
+        System.out.println("Student ID: " + STUDENT_ID);
+        System.out.println("Age: " + AGE);
+        System.out.println("Name: " + NAME);
+        System.out.println("Username: " + USERNAME);
     }
 }
